f158ef71ed7ef10d93ea3e70adb879a05c95f2b1
hexahedria <hexahedria@gmail.com>
2019-06-16 17:18:53 -0700

    Visual tweaks

--- src/spiralclock.c
+++ src/spiralclock.c
@@ -84,17 +84,22 @@ static void make_arc(FContext *fctx, FPoint center, fixed_t start_r, fixed_t end
 }
 
 #define SCALE_RADIUS(start_ang, end_ang, cur_ang, start_r, end_r) (start_r + ((end_r-start_r)*(cur_ang-start_ang))/(end_ang-start_ang))
-static void make_spiral(FContext *fctx, FPoint center, fixed_t halfwidth, fixed_t start_r, fixed_t end_r, int32_t start_angle, int32_t end_angle, int32_t subdivisions){
+static void make_spiral_dyn_width(FContext *fctx, FPoint center, fixed_t start_halfwidth, fixed_t end_halfwidth, fixed_t start_r, fixed_t end_r, int32_t start_angle, int32_t end_angle, int32_t subdivisions){
   bool positive_radians = end_angle >= start_angle;
   int32_t quad_angle = TRIG_MAX_ANGLE/subdivisions;
   int32_t flip = (positive_radians ? 1 : -1);
   int32_t quad_step = flip * quad_angle;
+
+  fixed_t start_rph = start_r + start_halfwidth;
+  fixed_t start_rmh = start_r - start_halfwidth;
+  fixed_t end_rph = end_r + end_halfwidth;
+  fixed_t end_rmh = end_r - end_halfwidth;
   
-  fctx_move_to(fctx, get_angular_point(center, start_r+halfwidth, start_angle));
+  fctx_move_to(fctx, get_angular_point(center, start_rph, start_angle));
   if(flip*(end_angle - start_angle) <= quad_angle){
-    make_arc(fctx, center, start_r+halfwidth, end_r+halfwidth, start_angle, end_angle);
-    fctx_line_to(fctx, get_angular_point(center, end_r-halfwidth, end_angle));
-    make_arc(fctx, center, end_r-halfwidth, start_r-halfwidth, end_angle, start_angle);
+    make_arc(fctx, center, start_rph, end_rph, start_angle, end_angle);
+    fctx_line_to(fctx, get_angular_point(center, end_rmh, end_angle));
+    make_arc(fctx, center, end_rmh, start_rmh, end_angle, start_angle);
   }else{
     int32_t start_delta = start_angle % quad_angle;
     if(start_delta<0)
@@ -107,51 +112,64 @@ static void make_spiral(FContext *fctx, FPoint center, fixed_t halfwidth, fixed_
       cur_angle = start_angle - start_delta;
     
     make_arc(fctx, center,
-             start_r+halfwidth,
-             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_r,end_r)+halfwidth,
+             start_rph,
+             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_rph,end_rph),
              start_angle, cur_angle);
     
     int32_t stop_angle = end_angle - quad_step;
     for(; flip*cur_angle < flip*stop_angle; cur_angle += quad_step){
       make_arc(fctx, center,
-               SCALE_RADIUS(start_angle,end_angle,cur_angle,start_r,end_r)+halfwidth,
-               SCALE_RADIUS(start_angle,end_angle,cur_angle+quad_step,start_r,end_r)+halfwidth,
+               SCALE_RADIUS(start_angle,end_angle,cur_angle,start_rph,end_rph),
+               SCALE_RADIUS(start_angle,end_angle,cur_angle+quad_step,start_rph,end_rph),
                cur_angle, cur_angle+quad_step);
     }
     
     make_arc(fctx, center,
-             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_r,end_r)+halfwidth,
-             end_r+halfwidth,
+             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_rph,end_rph),
+             end_rph,
              cur_angle, end_angle);
     
-    fctx_line_to(fctx, get_angular_point(center, end_r-halfwidth, end_angle));
+    fctx_line_to(fctx, get_angular_point(center, end_rmh, end_angle));
     
     make_arc(fctx, center,
-             end_r-halfwidth,
-             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_r,end_r)-halfwidth,
+             end_rmh,
+             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_rmh,end_rmh),
              end_angle, cur_angle);
     
     stop_angle = start_angle + quad_step;
     for(; flip*cur_angle > flip*stop_angle; cur_angle -= quad_step){
       make_arc(fctx, center,
-               SCALE_RADIUS(start_angle,end_angle,cur_angle,start_r,end_r)-halfwidth,
-               SCALE_RADIUS(start_angle,end_angle,cur_angle-quad_step,start_r,end_r)-halfwidth,
+               SCALE_RADIUS(start_angle,end_angle,cur_angle,start_rmh,end_rmh),
+               SCALE_RADIUS(start_angle,end_angle,cur_angle-quad_step,start_rmh,end_rmh),
                cur_angle, cur_angle-quad_step);
     }
     
     make_arc(fctx, center,
-             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_r,end_r)-halfwidth,
-             start_r-halfwidth,
+             SCALE_RADIUS(start_angle,end_angle,cur_angle,start_rmh,end_rmh),
+             start_rmh,
              cur_angle, start_angle);
   }
   
   fctx_close_path(fctx);
 }
+static void make_spiral(FContext *fctx, FPoint center, fixed_t halfwidth, fixed_t start_r, fixed_t end_r, int32_t start_angle, int32_t end_angle, int32_t subdivisions){
+  make_spiral_dyn_width(fctx, center, halfwidth, halfwidth, start_r, end_r, start_angle, end_angle, subdivisions);
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-overflow"
 static void make_spiral_rate(FContext *fctx, FPoint center, fixed_t halfwidth, fixed_t start_r, fixed_t delta_per_revolution, int32_t start_angle, int32_t end_angle, int32_t subdivisions){
   bool positive_radians = end_angle >= start_angle;
   fixed_t end_r = start_r + ((positive_radians ? 1 : -1) * (end_angle - start_angle) * delta_per_revolution) / TRIG_MAX_ANGLE;
   make_spiral(fctx, center, halfwidth, start_r, end_r, start_angle, end_angle, subdivisions);
 }
+static void make_spiral_rate_dyn_width(FContext *fctx, FPoint center, fixed_t start_halfwidth, fixed_t start_r, fixed_t d_r_per_rev, fixed_t d_h_per_rev, int32_t start_angle, int32_t end_angle, int32_t subdivisions){
+  bool positive_radians = end_angle >= start_angle;
+  fixed_t end_r = start_r + ((positive_radians ? 1 : -1) * (end_angle - start_angle) * d_r_per_rev) / TRIG_MAX_ANGLE;
+  fixed_t end_halfwidth = start_halfwidth + ((positive_radians ? 1 : -1) * (end_angle - start_angle) * d_h_per_rev) / TRIG_MAX_ANGLE;
+  make_spiral_dyn_width(fctx, center, start_halfwidth, end_halfwidth, start_r, end_r, start_angle, end_angle, subdivisions);
+}
+#pragma GCC diagnostic pop
 
 static void draw_spirals_for_time(struct Layer *layer, FContext *fctx, int32_t time, int32_t time_per_twelve){
   
@@ -159,28 +177,80 @@ static void draw_spirals_for_time(struct Layer *layer, FContext *fctx, int32_t t
   
   GRect bounds = layer_get_bounds(layer);
   
-  fixed_t minute_outer_radius = INT_TO_FIXED(bounds.size.w/2 - 5);
-  fixed_t minute_radius_shift = -(minute_outer_radius)/13;
+  fixed_t minute_outer_radius = INT_TO_FIXED(bounds.size.w/2 - 11);
+  fixed_t minute_radius_shift = -(minute_outer_radius)/14;
   fixed_t minute_halfwidth = (minute_outer_radius)/50;
+
+  fixed_t minute_halfwidth_first_boost = minute_halfwidth * 3 / 2;
+  fixed_t minute_radius_first_boost = minute_halfwidth_first_boost;
+
+  fixed_t minute_radius_shift_first = minute_radius_shift - minute_radius_first_boost;
     
-  fixed_t hour_outer_radius = minute_outer_radius + minute_radius_shift*3/2;
+  fixed_t hour_outer_radius = minute_outer_radius + minute_radius_shift_first*3/2;
   fixed_t hour_halfwidth = INT_TO_FIXED(2);
   fixed_t hour_cover_halfwidth = INT_TO_FIXED(9)/2;
-  fixed_t hour_cover_angle_boost = 0;
+  fixed_t hour_cover_angle_boost = TRIG_MAX_ANGLE/120;
+  fixed_t hour_cover_inner_hw_boost = hour_cover_halfwidth / 3;
   
   FPoint center = FPointI(bounds.origin.x+bounds.size.w/2, bounds.origin.y+bounds.size.h/2);
   
-  fixed_t tick_halfwidth = INT_TO_FIXED(1);
+  fixed_t tick_halfwidth = INT_TO_FIXED(3)/2;
   fixed_t tick_length = INT_TO_FIXED(3);
-  fixed_t tick_inner_radius = minute_outer_radius+INT_TO_FIXED(2);
+  fixed_t tick_inner_radius = minute_outer_radius+INT_TO_FIXED(3);
   
   // make inverse minutes spiral
   fctx_set_fill_color(fctx, MINUTE_COVER_COLOR);
   
   fctx_begin_fill(fctx);
-  start = -(TRIG_MAX_ANGLE/4) + (TRIG_MAX_ANGLE * time * 12) / time_per_twelve;
-  end = -(TRIG_MAX_ANGLE/4);
-  make_spiral_rate(fctx, center, minute_halfwidth, (minute_outer_radius), (minute_radius_shift), start, end, 4);
+  if (time * 12 < time_per_twelve) {
+    // Less than one revolution
+    start = -(TRIG_MAX_ANGLE/4) + (TRIG_MAX_ANGLE * time * 12) / time_per_twelve;
+    end = -(TRIG_MAX_ANGLE/4);
+    make_spiral_rate_dyn_width(
+      fctx,
+      center,
+      minute_halfwidth + minute_halfwidth_first_boost,
+      minute_outer_radius,
+      minute_radius_shift_first,
+      -minute_halfwidth_first_boost,
+      start,
+      end,
+      4);
+    inner_rad = minute_outer_radius
+      + (minute_radius_shift_first * time * 12 ) / time_per_twelve;
+  } else {
+    // More than one revolution
+    // First revolution
+    start = -(TRIG_MAX_ANGLE/4) + (TRIG_MAX_ANGLE * time * 12) / time_per_twelve;
+    end = start - TRIG_MAX_ANGLE;
+    make_spiral_rate_dyn_width(
+      fctx,
+      center,
+      minute_halfwidth + minute_halfwidth_first_boost,
+      minute_outer_radius,
+      minute_radius_shift_first,
+      -minute_halfwidth_first_boost,
+      start,
+      end,
+      4);
+    // Other revolutions
+    start = end;
+    end = -(TRIG_MAX_ANGLE/4);
+    make_spiral_rate(
+      fctx,
+      center,
+      minute_halfwidth,
+      minute_outer_radius + minute_radius_shift_first,
+      minute_radius_shift,
+      start,
+      end,
+      4);
+    inner_rad = minute_outer_radius
+      + minute_radius_shift_first
+      - minute_radius_shift
+      + (minute_radius_shift * time * 12) / time_per_twelve;
+  }
+  // Outer box
   fctx_move_to(fctx, FPointI(bounds.origin.x, bounds.origin.y));
   fctx_line_to(fctx, FPointI(bounds.origin.x+bounds.size.w, bounds.origin.y));
   fctx_line_to(fctx, FPointI(bounds.origin.x+bounds.size.w, bounds.origin.y+bounds.size.h));
@@ -188,7 +258,6 @@ static void draw_spirals_for_time(struct Layer *layer, FContext *fctx, int32_t t
   fctx_close_path(fctx);
   fctx_end_fill(fctx);
   
-  inner_rad = minute_outer_radius + (minute_radius_shift * time * 12 ) / time_per_twelve;
   if(inner_rad > hour_outer_radius)
     inner_rad = hour_outer_radius;
   start = -(TRIG_MAX_ANGLE/4) + (TRIG_MAX_ANGLE * time ) / time_per_twelve;
@@ -196,7 +265,29 @@ static void draw_spirals_for_time(struct Layer *layer, FContext *fctx, int32_t t
   
   // make hours cover spiral
   fctx_begin_fill(fctx);
-  make_spiral(fctx, center, hour_cover_halfwidth, (hour_outer_radius), (inner_rad), start+hour_cover_angle_boost, end, 32);
+  make_spiral_dyn_width(
+    fctx,
+    center,
+    hour_cover_halfwidth + hour_cover_inner_hw_boost,
+    hour_cover_halfwidth,
+    (hour_outer_radius - hour_cover_inner_hw_boost),
+    (inner_rad),
+    start,
+    end,
+    32);
+  fctx_end_fill(fctx);
+  // small extra boost to give contrast
+  fctx_begin_fill(fctx);
+  make_spiral_dyn_width(
+    fctx,
+    center,
+    hour_cover_halfwidth + hour_cover_inner_hw_boost,
+    hour_cover_halfwidth + hour_cover_inner_hw_boost,
+    (hour_outer_radius - hour_cover_inner_hw_boost),
+    (hour_outer_radius - hour_cover_inner_hw_boost),
+    start+hour_cover_angle_boost,
+    start,
+    32);
   fctx_end_fill(fctx);
   
   fctx_set_fill_color(fctx, HOUR_SPIRAL_COLOR);
@@ -210,12 +301,20 @@ static void draw_spirals_for_time(struct Layer *layer, FContext *fctx, int32_t t
   fctx_set_fill_color(fctx, TICK_COLOR);
   fctx_begin_fill(fctx);
   fctx_set_offset(fctx, center);
+  fixed_t clen, chw;
   for(int i=0; i<12; i++){
+    if (i == 9) {
+      clen = tick_length * 2;
+      chw = tick_halfwidth * 2;
+    } else {
+      clen = tick_length;
+      chw = tick_halfwidth;
+    }
     fctx_set_rotation(fctx, TRIG_MAX_ANGLE * i / 12);
-    fctx_move_to(fctx, FPoint(tick_inner_radius, -tick_halfwidth));
-    fctx_line_to(fctx, FPoint(tick_inner_radius+tick_length, -tick_halfwidth));
-    fctx_line_to(fctx, FPoint(tick_inner_radius+tick_length, +tick_halfwidth));
-    fctx_line_to(fctx, FPoint(tick_inner_radius, +tick_halfwidth));
+    fctx_move_to(fctx, FPoint(tick_inner_radius, -chw));
+    fctx_line_to(fctx, FPoint(tick_inner_radius+clen, -chw));
+    fctx_line_to(fctx, FPoint(tick_inner_radius+clen, +chw));
+    fctx_line_to(fctx, FPoint(tick_inner_radius, +chw));
     fctx_close_path(fctx);
   }
   fctx_end_fill(fctx);
@@ -227,7 +326,8 @@ static void spiral_update_proc(struct Layer *layer, GContext *ctx){
   FContext _fctx, *fctx = &_fctx;
   fctx_init_context(fctx, ctx);
   
-  draw_spirals_for_time(layer, fctx, minutes_ct, 12*60);
+  // draw_spirals_for_time(layer, fctx, minutes_ct, 12*60);
+  draw_spirals_for_time(layer, fctx, 1*60 + 37, 12*60);
   
   fctx_deinit_context(fctx);
 }
